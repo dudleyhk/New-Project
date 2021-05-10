ad9dfb1500b5ee1128272042d5798807eefbdfce
induceinnovation <dudleyharvey-kelly@hotmail.com>
2021-05-10 17:08:18 +0100

    Message

--- Assets/Scripts/MakeTriggerFireEvent.cs
+++ Assets/Scripts/MakeTriggerFireEvent.cs
@@ -2,22 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-
-
-
-public class PlayCubeAnimation : MonoBehaviour
+public class MakeTriggerFireEvent : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            Debug.Log("Player has entered trigger area");
+        }
     }
 }
